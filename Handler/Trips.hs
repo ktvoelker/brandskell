854952e4471c3f7bd7934ab3e9a4a0b3c1f2de38
Derek Gonyeo <dgonyeo@csh.rit.edu>
2015-08-10 22:48:09 -0400

    Initial parts of the trips page. Currently has linker issues.

---
+++ Handler/Trips.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE QuasiQuotes, ScopedTypeVariables #-}
+module Handler.Trips where
+
+import Import
+import Utils.Database
+
+import qualified Hasql as H
+import qualified Data.Text as T
+
+pageSize :: Int
+pageSize = 10
+
+getTripsR :: Int -> Handler Html
+getTripsR page
+    | page < 0 = getTripsR 0
+    | otherwise = do
+        dbres <- liftIO $ do
+            conn <- getDbConn
+            H.session conn $ H.tx Nothing $ do
+                (trips :: [(Int,Day,Day,Int,T.Text)])
+                    <- H.listEx $ [H.stmt|
+                        SELECT trip.id             AS trip_id
+                             , min(date_start)     AS date_start
+                             , max(date_end)       AS date_end
+                             , count(*)            AS num_attendees
+                             , min(reasons.reason) AS reason
+                        FROM trips
+                        INNER JOIN entries ON (trips.id = entries.trip_id)
+                        INNER JOIN reasons ON (trips.reason_id = reasons.id)
+                        GROUP BY trips.id
+                        ORDER BY min(date_start) DESC
+                        LIMIT ?
+                        OFFSET ?
+                    |] pageSize (page * pageSize)
+                Identity num_trips <- H.singleEx [H.stmt|
+                        SELECT count(*) FROM trips
+                    |]
+                return (trips, num_trips)
+        case dbres of
+            Left err -> error $ show err -- TODO
+            Right (trips,num_trips) ->
+                    defaultLayout $ do
+                        let pages = [0..(num_trips `div` pageSize + 1)]
+                        setTitle "Brandreth Guestbook"
+                        $(widgetFile "trips")
--- Utils/Database.hs
+++ Utils/Database.hs
@@ -1,41 +1,10 @@
 module Utils.Database where
 
+import Import
 import Data.Maybe
-import Data.Time.Calendar
 
 import qualified Hasql as H
 import qualified Hasql.Postgres as HP
-import qualified Data.Text as T
-
-data Reason = Reason { reasonId   :: Int
-                     , reasonText :: T.Text
-                     } deriving(Show,Eq)
-
-data Trip = Trip { tripId       :: Int
-                 , tripReasonId :: Int
-                 } deriving(Show,Eq)
-
-data Source = Source { sourceId   :: Int
-                     , sourceText :: T.Text
-                     } deriving(Show,Eq)
-
-data Person = Person { personId       :: Int
-                     , personName     :: T.Text
-                     , personNick     :: Maybe T.Text
-                     , personUsername :: Maybe T.Text
-                     , personSourceId :: Int
-                     } deriving(Show,Eq)
-
-data Entry = Entry { entryPersonId :: Int
-                   , entryTripId   :: Int
-                   , entryStart    :: Day
-                   , entryEnd      :: Day
-                   , entryEntry    :: T.Text
-                   , entryImage    :: Maybe T.Text
-                   } deriving(Show,Eq)
-
-poolMVar :: MVar (H.Pool HP.Postgres)
-poolMVar = newEmptyMVar
 
 postgresSettings :: HP.Settings
 postgresSettings = HP.ParamSettings "localhost"      -- Host
@@ -50,25 +19,4 @@ poolSettings :: H.PoolSettings
 poolSettings = fromJust $ H.poolSettings 6 30
 
 getDbConn :: IO (H.Pool HP.Postgres)
-getDbConn = do
-    isEmpty <- isEmptyMVar poolMVar
-    if isEmpty
-        then do pool <- H.acquirePool postgresSettings poolSettings
-                putMVar poolMVar pool
-                return pool
-        else readMVar poolMVar
-
-toReason :: (Int,T.Text) -> Reason
-toReason (rid,rtxt) = Reason rid rtxt
-
-toTrip :: (Int,Int) -> Trip
-toTrip (tid,rid) = Trip tid rid
-
-toSource :: (Int,T.Text)
-toSource (sid,stxt) = Source sid stxt
-
-toPerson :: (Int,T.Text,Maybe T.Text,Maybe T.Text,Int)
-toPerson (pid,na,ni,us,src) = Person pid na ni us src
-
-toEntry :: (Int,Int,Day,Day,T.Text,Maybe T.Text)
-toEntry (pid,tid,st,en,entry,img) = Entry pid tid st en entry img
+getDbConn = H.acquirePool postgresSettings poolSettings
