9f9732ea541b38ccda70f34181473e7171876706
Derek Gonyeo <dgonyeo@csh.rit.edu>
2015-08-13 17:56:17 -0400

    Tweaked the schema to allow for an arbitrary amount of pictures per entry

--- init_db.sql
+++ init_db.sql
@@ -1,10 +1,13 @@
 BEGIN; 
 
+DROP TABLE IF EXISTS "images" CASCADE;
+DROP TABLE IF EXISTS "entries" CASCADE;
 DROP TABLE IF EXISTS "people" CASCADE;
 DROP TABLE IF EXISTS "trips" CASCADE;
-DROP TABLE IF EXISTS "entries" CASCADE;
 DROP TABLE IF EXISTS "sources" CASCADE;
 DROP TABLE IF EXISTS "reasons" CASCADE;
+
+DROP TYPE IF EXISTS img_type;
 
 CREATE TABLE "reasons" (
     id     SERIAL       NOT NULL,
@@ -35,15 +38,27 @@ CREATE TABLE "people" (
 );
 
 CREATE TABLE "entries" (
+    id         SERIAL  NOT NULL,
     person_id  INTEGER NOT NULL,
     trip_id    INTEGER NOT NULL,
     date_start DATE    NOT NULL,
     date_end   DATE    NOT NULL,
     entry      TEXT    NOT NULL,
     image_path TEXT,
-    PRIMARY KEY (person_id, trip_id),
+    PRIMARY KEY (id),
     FOREIGN KEY (person_id) REFERENCES "people" (id) ON DELETE CASCADE,
     FOREIGN KEY (trip_id) REFERENCES "trips" (id) ON DELETE CASCADE
+);
+
+CREATE TYPE img_type AS ENUM ('entry', 'doodle');
+
+CREATE TABLE "images" (
+    id         SERIAL   NOT NULL,
+    entry_id   INTEGER  NOT NULL,
+    image_path TEXT     NOT NULL,
+    img_type   img_type NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (entry_id) REFERENCES "entries" (id) ON DELETE CASCADE
 );
 
 COMMIT;
